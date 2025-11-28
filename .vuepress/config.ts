c382ec0fdc5bc6dde40105652403ca3eca6a4dbc
jsonzhao <json.zhao.cn@outlook.com>
2025-11-28 14:32:21 +0800

    rrr

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -15,7 +15,7 @@ export default defineUserConfig({
       server: {
         // Ignore large folders to avoid expensive file watch on Windows (node_modules/.git/.vuepress/.temp)
         watch: {
-          ignored: ['**/node_modules/**', '**/.git/**', '**/.vuepress/.temp/**']
+            ignored: ['**/node_modules/**', '**/.git/**', '**/.vuepress/.temp/**']
         }
       },
       // If some dependencies cause slow pre-bundling, list them here to exclude from optimizeDeps
@@ -29,12 +29,10 @@ export default defineUserConfig({
     logo: "/logo.png",
     author: "json zhao",
     authorAvatar: "/head.png",
-    //Github repository
-    //docsRepo: "https://github.com/vuepress-reco/vuepress-theme-reco-next",
     docsBranch: "main",
     docsDir: "example",
     lastUpdatedText: "",
-    // series 为原 sidebar
+    // series as sidebar
     // series: {
     //   // "/docs/theme-reco/": [
     //   //   {
@@ -55,9 +53,6 @@ export default defineUserConfig({
       { text: "MES", link: "/categories/MES/1.html" },
       { text: "WMS", link: "/categories/WMS/1.html" },
       { text: "Project", link: "/categories/Project/1.html" },
-      //{ text: "Digitalization", link: "/categories/Digitalization/1.html" },
-      //{ text: "Experiences", link: "/categories/Experience/1.html" },
-      // Use the tags index instead of a hard-coded tag page to avoid 404s
       { text: "Tags", link: "/tags/SQLServer/1.html" },
       // {
       //   text: "Docs",
@@ -66,60 +61,37 @@ export default defineUserConfig({
       //     { text: "vuepress-theme-reco", link: "/blogs/other/guide" },
       //   ],
       // },
-    // bulletin: {
-    //   body: [
-    //     {
-    //       type: "text",
-    //       content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
-    //       style: "font-size: 12px;",
-    //     },
-    //     {
-    //       type: "hr",
-    //     },
-    //     {
-    //       type: "title",
-    //       content: "QQ 群",
-    //     },
-    //     {
-    //       type: "text",
-    //       content: `
-    //       <ul>
-    //         <li>QQ群1:1037296104</li>
-    //         <li>QQ群2:1061561395</li>
-    //         <li>QQ群:962687802</li>
-    //       </ul>`,
-    //       style: "font-size: 12px;",
-    //     },
-    //     {
-    //       type: "hr",
-    //     },
-    //     {
-    //       type: "title",
-    //       content: "GitHub",
-    //     },
-    //     {
-    //       type: "text",
-    //       content: `
-    //       <ul>
-    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
-    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
-    //       </ul>`,
-    //       style: "font-size: 12px;",
-    //     },
-    //     {
-    //       type: "hr",
-    //     },
-    //     {
-    //       type: "buttongroup",
-    //       children: [
-    //         {
-    //           text: "打赏",
-    //           link: "/docs/others/donate.html",
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // },
+    ],
+    bulletin: {
+      title:'Information',
+      body: [
+        {
+          type: "text",
+          content: `I write this blog just for share some Manifacturing Industry experiences and skills,and I hope it can help you.<br>
+          If you have any questions, please contact me.`,
+          style: "font-size: 12px;",
+        },
+        {
+          type: "hr",
+        },
+        {
+          type: "title",
+          content: "Contact",
+        },
+        {
+          type: "text",
+          content: `
+          <ul>
+            <li>Email:json.zhao.cn@outlook.com</li>
+            <li>Phone:+86 13404849141</li>
+          </ul>`,
+          style: "font-size: 12px;",
+        },
+        {
+          type: "hr",
+        },
+      ],
+    },
     // commentConfig: {
     //   type: 'valine',
     //   // options 与 1.x 的 valineConfig 配置一致
@@ -133,6 +105,6 @@ export default defineUserConfig({
     //     // hideComments: true // 隐藏评论
     //   },
     // },
-  ]}),
-  // debug: true,
+  }),
+  //debug: true,
 });
